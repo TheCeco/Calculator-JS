47a7bdb3fcfede3d4e43e579556c66012d879876
TheCeco <rapidfire114@gmail.com>
2023-08-22 01:17:27 +0300

    added some error textures, point for float numbers and change some colors

--- calculator.js
+++ calculator.js
@@ -1,14 +1,27 @@
 const input = document.getElementById('number-input');
 
 input.addEventListener('input', function () {
-    console.log(input.value)
-});
+    const pattern = /^([\d]+)(\.?)([\d]+)$/
+
+    if (!input.value.match(pattern) && input.value.length > 0){
+        input.style.border = '1px solid red' 
+        input.style.boxShadow = '0 0 5px red'
+    } else {
+        input.style.border = ''
+        input.style.boxShadow = ''
+    }
+})
+
 
 const numbers = document.getElementsByClassName('number');
 
 for (let i = 0; i < numbers.length; i++){
     numbers[i].addEventListener('click', function () {
-        input.value += numbers[i].children[0].textContent
+        if (input.value.startsWith('0')) {
+            input.value = numbers[i].children[0].textContent
+        } else {
+            input.value += numbers[i].children[0].textContent
+        }    
     });
 };
 
@@ -24,3 +37,9 @@ operations[1].addEventListener('click', function() {
     result = num / 100;
     input.value = String(result);
 });
+
+operations[6].addEventListener('click', function() {
+    if (input.value.length > 0 && !input.value.includes('.')) {
+        input.value = input.value + '.'
+    }
+})
