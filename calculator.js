bd80eccf16783b88d4aee12e9636ac7b5fe976c9
TheCeco <rapidfire114@gmail.com>
2023-08-22 15:43:44 +0300

    Operations functionality have been added. Tried to catch some errors.

--- calculator.js
+++ calculator.js
@@ -1,45 +1,128 @@
-const input = document.getElementById('number-input');
+function do_operation(operation, num1, num2) {
+  if (operation === "/") {
+    return num1 / num2;
+  } else if (operation === "*") {
+    return num1 * num2;
+  } else if (operation === "+") {
+    return num1 + num2;
+  } else if (operation === "-") {
+    return num1 - num2;
+  }
+}
 
-input.addEventListener('input', function () {
-    const pattern = /^([\d]+)(\.?)([\d]+)$/
+let num1 = 0;
+let num2 = 0;
 
-    if (!input.value.match(pattern) && input.value.length > 0){
-        input.style.border = '1px solid red' 
-        input.style.boxShadow = '0 0 5px red'
+let operation = "";
+
+const operationsDiv = document.getElementsByClassName("operation");
+
+const input = document.getElementById("number-input");
+
+input.addEventListener("input", function () {
+  const pattern = /^-?\d+(\.\d+)?$/;
+
+  if (!input.value.match(pattern) && input.value !== "") {
+    input.style.border = "1px solid red";
+    input.style.boxShadow = "0 0 5px red";
+
+    for (let i = 1; i < operationsDiv.length; i++) {
+      operationsDiv[i].children[0].disabled = true;
+    }
+  } else {
+    input.style.border = "";
+    input.style.boxShadow = "";
+
+    for (let i = 1; i < operationsDiv.length; i++) {
+      operationsDiv[i].children[0].disabled = false;
+    }
+  }
+});
+
+const numberBtns = document.getElementsByClassName("number");
+const pattern = /^[0]$/;
+for (let i = 0; i < numberBtns.length; i++) {
+  numberBtns[i].addEventListener("click", function () {
+    if (operation) {
+      if (Number(input.value) === num1) {
+        input.value = numberBtns[i].children[0].textContent;
+      } else {
+        input.value += numberBtns[i].children[0].textContent;
+      }
     } else {
-        input.style.border = ''
-        input.style.boxShadow = ''
+      if (input.value.length === 1 && input.value.match(pattern)) {
+        input.value = numberBtns[i].children[0].textContent;
+      } else {
+        input.value += numberBtns[i].children[0].textContent;
+      }
     }
-})
+  });
+}
 
+const clearBtn = operationsDiv[0];
+clearBtn.addEventListener("click", clearAll);
+function clearAll(event, message = "") {
+  input.value = message;
 
-const numbers = document.getElementsByClassName('number');
+  input.style.border = "";
+  input.style.boxShadow = "";
 
-for (let i = 0; i < numbers.length; i++){
-    numbers[i].addEventListener('click', function () {
-        if (input.value.startsWith('0')) {
-            input.value = numbers[i].children[0].textContent
-        } else {
-            input.value += numbers[i].children[0].textContent
-        }    
-    });
-};
+  num1 = 0;
+  num2 = 0;
+  operation = "";
 
-const operations = document.getElementsByClassName('operation')
+  for (let i = 1; i < operationsDiv.length; i++) {
+    operationsDiv[i].children[0].disabled = false;
+  }
+}
 
-operations[0].addEventListener('click', function() {
-    input.value = '';
+const persentBtn = operationsDiv[1];
+persentBtn.addEventListener("click", function () {
+  num1 = Number(input.value);
+  result = num1 / 100;
+  console.log(num1);
+  input.value = String(result);
+  num1 = Number(input.value);
 });
 
-operations[1].addEventListener('click', function() {
-    num = Number(input.value);
-    
-    result = num / 100;
-    input.value = String(result);
+// Giving every operation a functionality
+for (let i = 2; i < 6; i++) {
+  operationsDiv[i].addEventListener("click", function () {
+    if (operation) {
+      num2 = Number(input.value);
+      input.value = "";
+      num1 = do_operation(operation, num1, num2);
+
+      if (isNaN(num1) || num1 === Infinity) {
+        clearAll(event, (message = "Error"));
+        return;
+      }
+
+      input.value = String(num1);
+    } else {
+      num1 = Number(input.value);
+      input.value = "";
+      operation = operationsDiv[i].children[0].textContent;
+    }
+  });
+}
+
+const floatBtn = operationsDiv[6];
+floatBtn.addEventListener("click", function () {
+  if (input.value.length > 0 && !input.value.includes(".")) {
+    input.value = input.value + ".";
+  }
 });
 
-operations[6].addEventListener('click', function() {
-    if (input.value.length > 0 && !input.value.includes('.')) {
-        input.value = input.value + '.'
-    }
-})
+const equalBtn = document.getElementById("equal-button");
+equalBtn.addEventListener("click", function () {
+  num2 = Number(input.value);
+  input.value = String(do_operation(operation, num1, num2));
+
+  if (isNaN(input.value) || input.value === Infinity) {
+    clearAll(event, (message = "Error"));
+    return;
+  }
+
+  operation = "";
+});
